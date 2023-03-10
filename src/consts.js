3f8a4454cd591a6bf0347c08754b3d10a0bec9ad
gongshang <xinyuzheng31@gmail.com>
2023-03-10 23:31:09 +0800

    第一次提交

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `Zachary's Blog`;
+export const SITE_DESCRIPTION = 'Zachary Site Description';
+export const SITE_EMAIL = 'xinyuzheng31@qq.com'
+export const SITE_NAME = 'igongshang.cn';
+export const SITE_URL = "https://igongshang.cn";
